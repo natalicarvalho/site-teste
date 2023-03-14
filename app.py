0532943eda847a97b88b3950db9470d0799725b1
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-03-14 17:19:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ menu = """
 <a href="/">Página inicial</a> | <a href="/musica">Música</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
-<font color="purple">Contato</font> 
 
 @app.route("/")
 def hello_world():
-  return menu + "Olá, mundo! Esse é meu site. (Natali Carvalho)"
+  return menu + "Olá, mundo! Este é meu site. (Natali Carvalho)"
 
 @app.route("/sobre")
 def sobre():
-  return menu + "aqui vai o conteúdo da página Sobre"
+  return menu + "Jornalista cearense"
 
 @app.route("/contato")
 def contato():
-  return menu + "aqui vai o conteúdo da página Contato"
+  return menu + "Natali Carvalho | (85) 99222-9958"
 
-
+@app.route("/textos")
+def textos():
+  return menu + "O presidente Lula disse, em evento com lideranças indígenas em Roraima, que a escravidão trouxe uma coisa boa ao Brasil: a miscigenação. No Twitter, lembraram o presidente que essa mistura de raças aconteceu com muita violência e abusos e que não há nada de bom nisso."
 
