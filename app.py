7b7a3c6b728fb6c0b3ecfb1ccc8f85a0692def83
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-03-13 21:50:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Esse é meu site. (Natali Carvalho)"
+
+@app.route("/sobre")
+def sobre():
+  return "aqui vai o conteúdo da página sobre"
+
+@app.route("/contato")
+def contato():
+  return "aqui vai o conteúdo da página contato"
