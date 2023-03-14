aef0a07684042e941e12f13c6ca38a33414377c2
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-03-13 22:16:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 menu = """
-<a href="/">Página inicial</a> | <a href="/sobre">sobre</a> | <a href="/contato">contato</a>
+<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
 
@@ -13,8 +13,8 @@ def hello_world():
 
 @app.route("/sobre")
 def sobre():
-  return "aqui vai o conteúdo da página sobre"
+  return "aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "aqui vai o conteúdo da página contato"
+  return "aqui vai o conteúdo da página Contato"
