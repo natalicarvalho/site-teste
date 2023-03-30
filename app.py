cc572ace54da9c8be576ee3c1b59d91f2099bc12
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-03-30 10:55:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from tchan import channelScraper
 app = Flask(__name__)
 
 def ultimas_promocoes():
-  scraper = channelScraper()
+  scraper = ChannelScraper()
   contador = 0
   resultado = []
-  for message in scraper.messages("promocoeseachadinhos")
-  contador += 1
-  texto = message.text.strip().splitlines
-  resultado.append(f"{message.created_at} {texto}")
-  if contador == 10:
-    return resultado
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
 
 menu = """
 <a href="/">Página inicial</a> | <a href="/textos">Textos</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
@@ -35,13 +35,13 @@ def contato():
 def textos():
   return menu + "O presidente Lula disse, em evento com lideranças indígenas em Roraima, que a escravidão trouxe uma coisa boa ao Brasil: a miscigenação. No Twitter, lembraram o presidente que essa mistura de raças aconteceu com muita violência e abusos e que não há nada de bom nisso."
 
-@app.rout("/promocoes")
+@app.route("/promocoes")
 def promocoes():
-   conteudo = menu + """
-   Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos".@promocoeseachadinhos</a>
-   <br>
-   <ul>
-   """
-   for promocao in ultimas_promocoes():
-     conteudo += f"<li>(promocao}</li>"
-   return conteudo + "</ul>"
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  for promocao in ultimas_promocoes():
+    conteudo += f"<li>{promocao}</li>"
+  return conteudo + "</ul>"
