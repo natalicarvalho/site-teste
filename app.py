8c259a9ff175ac5c5160c7084b28458b6a1fee01
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-03-30 10:44:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from tchan import channelScraper
 
 app = Flask(__name__)
+
+def ultimas_promocoes():
+  scraper = channelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos")
+  contador += 1
+  texto = message.text.strip().splitlines
+  resultado.append(f"{message.created_at} {texto}")
+  if contador == 10:
+    return resultado
 
 menu = """
 <a href="/">Página inicial</a> | <a href="/textos">Textos</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
@@ -23,3 +35,13 @@ def contato():
 def textos():
   return menu + "O presidente Lula disse, em evento com lideranças indígenas em Roraima, que a escravidão trouxe uma coisa boa ao Brasil: a miscigenação. No Twitter, lembraram o presidente que essa mistura de raças aconteceu com muita violência e abusos e que não há nada de bom nisso."
 
+@app.rout("/promocoes")
+def promocoes():
+   conteudo = menu + """
+   Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos".@promocoeseachadinhos</a>
+   <br>
+   <ul>
+   """
+   for promocao in ultimas_promocoes():
+     conteudo += f"<li>(promocao}</li>"
+   return conteudo + "</ul>"
