f4881e27c3731f88a76de1971d6987ace83cc311
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-04-14 07:27:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,42 @@
-from flask import Flask
-from tchan import channelScraper
+import os
 
+import gspread
+import requests
+from flask import Flask, request
+from oauth2client.service_account import ServiceAccountCredentials
+from tchan import ChannelScraper
+
+from scraper import ultimas_promocoes
+
+
+TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
+TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
+GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+with open("credenciais.json", mode="w") as arquivo:
+  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+api = gspread.authorize(conta)
+planilha = api.open_by_key("1ZDyxhXlCtCjMbyKvYmMt_8jAKN5JSoZ7x3MqlnoyzAM")
+sheet = planilha.worksheet("Sheet1")
 app = Flask(__name__)
-
-def ultimas_promocoes():
-  scraper = ChannelScraper()
-  contador = 0
-  resultado = []
-  for message in scraper.messages("promocoeseachadinhos"):
-    contador += 1
-    texto = message.text.strip().splitlines()[0]
-    resultado.append(f"{message.created_at} {texto}")
-    if contador == 10:
-      return resultado
-
+    
 menu = """
-<a href="/">Página inicial</a> | <a href="/textos">Textos</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
 
 @app.route("/")
-def hello_world():
-  return menu + "Olá, mundo! Este é meu site. (Natali Carvalho)"
+def index():
+  return menu + "Olá, mundo! Esse é o site da Natali Carvalho"
 
 @app.route("/sobre")
 def sobre():
-  return menu + "Jornalista cearense"
+  return menu + "Sou uma jornalista cearense formada pela Universidade Federal do Ceará"
 
 @app.route("/contato")
 def contato():
-  return menu + "Natali Carvalho | (85) 99222-9958"
+  return menu + "O meu email é natali.lima.carvalho@gmail.com"
 
-@app.route("/textos")
-def textos():
-  return menu + "O presidente Lula disse, em evento com lideranças indígenas em Roraima, que a escravidão trouxe uma coisa boa ao Brasil: a miscigenação. No Twitter, lembraram o presidente que essa mistura de raças aconteceu com muita violência e abusos e que não há nada de bom nisso."
 
 @app.route("/promocoes")
 def promocoes():
@@ -45,3 +48,63 @@ def promocoes():
   for promocao in ultimas_promocoes():
     conteudo += f"<li>{promocao}</li>"
   return conteudo + "</ul>"
+
+
+@app.route("/promocoes2")
+def promocoes2():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  scraper = ChannelScraper()
+  contador = 0
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    conteudo += f"<li>{message.created_at} {texto}</li>"
+    if contador == 10:
+      break
+  return conteudo + "</ul>"
+
+@app.route("/dedoduro")
+def dedoduro():
+  mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": "Alguém acessou a página dedo duro!"}
+  resposta = requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
+  return f"Mensagem enviada. Resposta ({resposta.status_code}): {resposta.text}"
+
+
+@app.route("/dedoduro2")
+def dedoduro2():
+  sheet.append_row(["Natali", "Carvalho", "a partir do Flask"])
+  return "Planilha escrita!"
+
+
+@app.route("/telegram-bot", methods=["POST"])
+def telegram_bot():
+  update = request.json
+  chat_id = update["message"]["chat"]["id"]
+  message = update["message"]["text"]
+  nova_mensagem = {
+    "chat_id": chat_id,
+    "text": f"Você enviou a mensagem: <b>{message}</b>",
+    "parse_mode": "HTML",
+  }
+  resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+  print(resposta.text)
+  return "ok"
+
+
+def enviar_foto():
+  # Esboço
+  ...
+  chart.save("chart.png")
+  with open("chart.png", mode="rb") as arquivo:
+    conteudo = arquivo.read()
+  mensagem = {"chat_id": ..., "caption": "gráfico X"}
+  files = {"photo": ("grafico.png", conteudo)}
+  resposta = requests.post(
+    f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendPhoto", 
+    data=mensagem, 
+    files=files,
+  )
